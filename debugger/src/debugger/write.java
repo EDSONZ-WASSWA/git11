ea42a621dee160b406eb865425cb416a80f28863
EDSONZ-WASSWA <edsonwasswa54@gmail.com>
2024-09-12 23:56:14 +0300

    This is one of by first projects of java

---
+++ debugger/src/debugger/write.java
@@ -0,0 +1,28 @@
+package debugger;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+class write implements Runnable
+{
+    holder hotel_ob;
+    write(holder hotel_ob)
+    {
+        this.hotel_ob=hotel_ob;
+    }
+    @Override
+    public void run() {
+          try{
+        FileOutputStream fout=new FileOutputStream("backup");
+        ObjectOutputStream oos=new ObjectOutputStream(fout);
+        oos.writeObject(hotel_ob);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in writing "+e);
+        }         
+        
+    }
+    
+}
+
